6a28b7d5a063ea5dc1b2247c1d35d18bf72625a3
Artur Rodrigues <artur190503@outlook.com>
2025-05-21 04:38:30 -0300

    chore: Updating front end dependencies

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // manter por segurança
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
